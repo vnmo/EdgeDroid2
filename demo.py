751cb0d74cafb1e0f639bd533d5822d14d464340
Manuel Olguín Muñoz <manuel@olguinmunoz.xyz>
2022-06-01 16:43:30 +0200

    demo is working

--- demo.py
+++ demo.py
@@ -17,16 +17,24 @@ from __future__ import annotations
 import socket
 import sys
 import uuid
-from multiprocessing import Condition, Event, Process
+from collections import deque
+from multiprocessing import Condition, Event, Pipe, Process
+from multiprocessing.connection import Connection
+from threading import Event as TEvent, Thread
+from typing import Deque
 
+import cv2
+import numpy as np
+import numpy.typing as npt
 from loguru import logger
 
 from edgedroid.load_generator.client.client import StreamSocketEmulation
 from edgedroid.load_generator.server.server import server
+from edgedroid.models import ModelFrame
 
 
 class ServerProcess(Process):
-    def __init__(self, unix_socket_addr: str, task_name: str = "square00"):
+    def __init__(self, unix_socket_addr: str, task_name: str = "test"):
         super(ServerProcess, self).__init__()
         self._sock_addr = unix_socket_addr
         self._task = task_name
@@ -61,7 +69,90 @@ class ServerProcess(Process):
             sock.close()
 
 
-def demo_loop(trace="test"):
+class ClientProcess(Process):
+    def __init__(
+        self,
+        task_name: str = "test",
+        neuroticism: float = 0.5,
+        fade_distance: int = 8,
+    ):
+        super(ClientProcess, self).__init__()
+
+        self._task = task_name
+        self._neuro = neuroticism
+        self._fade_dist = fade_distance
+        self._is_finished = Event()
+
+        self._frame_recv, self._frame_send = Pipe(duplex=False)
+        self._guidance_recv, self._guidance_send = Pipe(duplex=False)
+
+    @property
+    def frame_recv_pipe(self) -> Connection:
+        return self._frame_recv
+
+    @property
+    def guidance_recv_pipe(self) -> Connection:
+        return self._guidance_recv
+
+    def is_finished(self) -> bool:
+        return self._is_finished.is_set()
+
+    def _frame_callback(self, frame: ModelFrame) -> None:
+        self._frame_send.send((frame.frame_data.tolist(), frame.frame_tag))
+
+    def _guide_callback(
+        self, transition: bool, img: npt.NDArray, instruction: str
+    ) -> None:
+        if transition:
+            self._guidance_send.send((img.tolist(), instruction))
+
+    def run(self) -> None:
+        self._is_finished.clear()
+        emulation = StreamSocketEmulation(
+            neuroticism=self._neuro,
+            trace=self._task,
+            fade_distance=self._fade_dist,
+        )
+        socket_addr = f"/tmp/{uuid.uuid4()}.sock"
+
+        logger.info("Starting server process")
+        server_proc = ServerProcess(socket_addr, task_name=self._task)
+
+        try:
+            server_proc.start()
+            server_proc.wait_until_ready()
+
+            # connect the client socket
+            sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            try:
+                logger.info(f"Connecting to server socket on {socket_addr}")
+                sock.connect(socket_addr)
+                logger.success("Connection success, starting emulation")
+                emulation.emulate(
+                    sock,
+                    emit_cb=self._frame_callback,
+                    resp_cb=self._guide_callback,
+                )
+                logger.success("Emulation done!")
+            finally:
+                sock.close()
+        finally:
+            server_proc.join(timeout=1.0)
+            self._is_finished.set()
+
+
+def pipe2deque(pipe: Connection, dq: Deque[npt.NDArray, str], shutdown: TEvent) -> None:
+    while not shutdown.is_set():
+        if pipe.poll(timeout=0.01):
+            img, msg = pipe.recv()
+            dq.append((np.array(img, dtype=np.uint8), msg))
+
+
+def demo_loop(
+    task_name: str = "test",
+    view_width: int = 1200,
+    view_height: int = 300,
+):
     # enable logging
     logger.enable("edgedroid")
     logger.remove()
@@ -75,34 +166,76 @@ def demo_loop(trace="test"):
         catch=True,
     )
 
-    emulation = StreamSocketEmulation(
-        neuroticism=0.5,
-        trace=trace,
-        fade_distance=8,
+    # initial black images
+    current_guidance = np.zeros((view_height, view_width // 2, 3), dtype=np.uint8)
+    current_input = np.zeros((view_height, view_width // 2, 3), dtype=np.uint8)
+
+    def resize_add_text(img: npt.NDArray, text: str) -> npt.NDArray:
+        w = view_width // 2
+
+        img = cv2.resize(img, (w, view_height), cv2.INTER_AREA)
+        cv2.putText(
+            img=img,
+            text=f"{text[:50]}",
+            color=(0, 0, 255),
+            org=(view_height // 10, w // 10),
+            fontFace=cv2.FONT_HERSHEY_PLAIN,
+            fontScale=1,
+            thickness=1,
+        )
+        return img
+
+    client_proc = ClientProcess(task_name=task_name, neuroticism=0.5, fade_distance=8)
+
+    frame_dq: Deque[npt.NDArray, str] = deque(maxlen=1)
+    guide_dq: Deque[npt.NDArray, str] = deque(maxlen=1)
+    dq_thread_shutdown = TEvent()
+
+    frame_recv_t = Thread(
+        target=pipe2deque,
+        args=(
+            client_proc.frame_recv_pipe,
+            frame_dq,
+            dq_thread_shutdown,
+        ),
+    )
+    guide_recv_t = Thread(
+        target=pipe2deque,
+        args=(
+            client_proc.guidance_recv_pipe,
+            guide_dq,
+            dq_thread_shutdown,
+        ),
     )
 
-    socket_addr = f"/tmp/{uuid.uuid4()}.sock"
-
-    logger.info("Starting server process")
-
-    server_proc = ServerProcess(socket_addr, task_name=trace)
-
+    # ui loop
+    _ = cv2.namedWindow("DEMO")
     try:
-        server_proc.start()
-        server_proc.wait_until_ready()
+        frame_recv_t.start()
+        guide_recv_t.start()
+        client_proc.start()
 
-        # connect the client socket
-        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        try:
-            logger.info(f"Connecting to server socket on {socket_addr}")
-            sock.connect(socket_addr)
-            logger.success("Connection success, starting emulation")
-            emulation.emulate(sock)
-            logger.success("Emulation done!")
-        finally:
-            sock.close()
+        while not client_proc.is_finished():
+            try:
+                guidance, msg = guide_dq.popleft()
+                current_guidance = resize_add_text(guidance, msg)
+            except IndexError:
+                pass
+
+            try:
+                frame, tag = frame_dq.popleft()
+                current_input = resize_add_text(frame, tag)
+            except IndexError:
+                pass
+
+            output_img = np.concatenate((current_guidance, current_input), axis=1)
+            cv2.imshow("DEMO", output_img)
+            cv2.waitKey(25)
     finally:
-        server_proc.join(timeout=1.0)
+        client_proc.join()
+        dq_thread_shutdown.set()
+        frame_recv_t.join()
+        guide_recv_t.join()
 
 
 if __name__ == "__main__":
