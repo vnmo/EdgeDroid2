535c185dd2e03bc5ccb4c1371af2939970879bdc
Manuel Olguín Muñoz <manuel@olguinmunoz.xyz>
2022-10-20 00:15:02 -0400

    new exps

--- experiments/experiments.py
+++ experiments/experiments.py
@@ -101,5 +101,45 @@ experiments: Dict[str, Callable[[], ExperimentConfig]] = {
             "sampling_scheme": "adaptive-power-fitted-naive-exgaussian",
         },
     ),
+    "empirical-high-greedy": lambda: ExperimentConfig(
+        timing_model=EmpiricalExecutionTimeModel.from_default_data(neuroticism=1.0),
+        sampling_scheme=ZeroWaitFrameSamplingModel.from_default_data(),
+        metadata={"timing_model": "empirical-high", "sampling_scheme": "greedy"},
+    ),
+    "empirical-low-greedy": lambda: ExperimentConfig(
+        timing_model=EmpiricalExecutionTimeModel.from_default_data(neuroticism=0.0),
+        sampling_scheme=ZeroWaitFrameSamplingModel.from_default_data(),
+        metadata={"timing_model": "empirical-low", "sampling_scheme": "greedy"},
+    ),
+    "theoretical-gaussian-high-greedy": lambda: ExperimentConfig(
+        timing_model=TheoreticalExecutionTimeModel.from_default_data(
+            neuroticism=1.0, distribution=stats.exponnorm
+        ),
+        sampling_scheme=ZeroWaitFrameSamplingModel.from_default_data(),
+        metadata={
+            "timing_model": "theoretical-gaussian-high",
+            "sampling_scheme": "greedy",
+        },
+    ),
+    "theoretical-gaussian-low-greedy": lambda: ExperimentConfig(
+        timing_model=TheoreticalExecutionTimeModel.from_default_data(
+            neuroticism=0.0, distribution=stats.exponnorm
+        ),
+        sampling_scheme=ZeroWaitFrameSamplingModel.from_default_data(),
+        metadata={
+            "timing_model": "theoretical-gaussian-low",
+            "sampling_scheme": "greedy",
+        },
+    ),
+    "fitted-naive-gaussian-greedy": lambda: ExperimentConfig(
+        timing_model=FittedNaiveExecutionTimeModel.from_default_data(
+            distribution=stats.exponnorm
+        ),
+        sampling_scheme=ZeroWaitFrameSamplingModel.from_default_data(),
+        metadata={
+            "timing_model": "fitted-naive-gaussian",
+            "sampling_scheme": "greedy",
+        },
+    ),
 }
 __all__ = ["experiments"]
